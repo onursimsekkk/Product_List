af670760e7197fb9f36ae0bde4dc48cf3b2cb6ba
Onur Simsek <onur.simsekk3@gmail.com>
2021-05-14 03:13:14 +0300

    local storage added

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ class UI {
     <th><a href="#" class="delete">&#10006;</a></th>
     `;
     
-    // Add to table item
+    // Add 'tr' to table item
     list.appendChild(row);
   }
 
@@ -57,7 +57,55 @@ class UI {
   }
 }
 
-// Event Listeners
+// Local Storage Class
+class Storage {
+  static getProducts() {
+    let products;
+    if(localStorage.getItem('products') === null) {
+      products = [];
+    } else {
+      products = JSON.parse(localStorage.getItem('products'));
+    }
+
+    return products;
+  }
+
+  static displayProducts() {
+    const products = Storage.getProducts();
+
+    products.forEach(function(product){
+      const ui = new UI;
+
+      // Add product to UI
+      ui.addProductToList(product);
+    });
+  }
+
+  static addProduct(product) {
+    const products = Storage.getProducts();
+
+    products.push(product);
+
+    localStorage.setItem('products', JSON.stringify(products));
+  }
+
+  static removeProduct(productNo) {
+    const products = Storage.getProducts();
+
+    products.forEach(function(product, index){
+      if(product.productNo === productNo) {
+        products.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('products', JSON.stringify(products));
+}
+};
+
+// DOM Load Event for display from local storage
+document.addEventListener('DOMContentLoaded', Storage.displayProducts);
+
+// Event Listeners for Add Product
 document.getElementById('product-form').addEventListener('submit', 
 function(e) {
   // Form value'lara ulaşma
@@ -69,7 +117,7 @@ function(e) {
   //  Product objesi oluşturma
 const product = new Product(productName, productNo, price);
 
-// UI ' a ekleme
+// UI nesnesi
 const ui = new UI();
 
 // Validate 
@@ -79,6 +127,9 @@ if(productName === "" || productNo === "" || price === "") {
 } else {
   // Add product to list
   ui.addProductToList(product);
+
+  // Add product to Local Storage
+  Storage.addProduct(product);
   // Add Alert
   ui.showAlert("Product Added!", "success");
 
@@ -97,6 +148,11 @@ document.getElementById('product-list').addEventListener('click', function(e) {
 
   // Call Delete Function
   ui.deleteProduct(e.target);
+
+  // Remove From Local Storage
+  // (Reaching to the productNo for every item that we clicked to delete because the productNo should be unique.)
+  //(Her bir ürünün productNo'sunu alarak eşsiz bir seçici oluşturuyoruz.)
+  Storage.removeProduct(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
 
   // Alert for Remove
   ui.showAlert("Product Removed!", "success");
